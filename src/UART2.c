1f3777234f99519f2e6f53b5b55f4186db3099d9
DESKTOP-NP620ON\Nimfrodian <aljazpflaum@gmail.com>
2023-12-04 13:46:00 +0100

    can better simulate SDM120M

--- src/UART2.c
+++ src/UART2.c
@@ -24,7 +24,7 @@ void uart2_init(void)
     // Set MODBUS 1 parameters
     uart_param_config(UART_NUM_2, &uart2_config);
     // Install MODBUS 1 driver
-    uart_driver_install(UART_NUM_2, 1024, 1024, 0, NULL, 0);
+    uart_driver_install(UART_NUM_2, 1024*4, 1024*4, 0, NULL, 0);
 }
 
 // prepares the master message based on input data
@@ -85,158 +85,167 @@ static int modbus2Receiver(int UartNum, mb2MasterMsg* MasterMsg)
     return toReturn;
 }
 
+static bool compareMasterMsgs(mb2MasterMsg* msg1, mb2MasterMsg* msg2)
+{
+    bool compare = msg1->mbData.dataArr[0] == msg2->mbData.dataArr[0] && // check ID
+                   msg1->mbData.dataArr[1] == msg2->mbData.dataArr[1] && // check Func Code
+                   msg1->mbData.dataArr[2] == msg2->mbData.dataArr[2] && // address high
+                   msg1->mbData.dataArr[3] == msg2->mbData.dataArr[3];   // address low
+    return compare;
+}
+
 static void modbus2ParserAndComposer(mb2MasterMsg* MasterMsg, mb2SlaveMsg* SlaveMsg)
 {
     const float SDM120M_READ_VOLTAGE_V = 230.1;
     const float SDM120M_READ_CURRENT_A = 0.1;
     const float SDM120M_ACTIVE_POWER_W = 23.24;
-    const float SDM120M_APPARENT_POWER_VA = -1.0f;
-    const float SDM120M_REACTIVE_POWER_VAr = -1.0f;
-    const float SDM120M_POWER_FACTOR = -1.0f;
+    const float SDM120M_APPARENT_POWER_VA= 0.0f;
+    const float SDM120M_REACTIVE_POWER_VAr= 0.0f;
+    const float SDM120M_POWER_FACTOR= 0.2f;
     const float SDM120M_FREQUENCY = 50.01;
-    const float SDM120M_IMPORT_ACTIVE_ENERGY_kWh = -1.0f;
-    const float SDM120M_EXPORT_ACTIVE_ENERGY_kWh = -1.0f;
-    const float SDM120M_IMPORT_REACTIVE_ENERGY_kVArh = -1.0f;
-    const float SDM120M_EXPORT_REACTIVE_ENERGY_kVArh = -1.0f;
-    const float SDM120M_TOTAL_SYSTEM_POWER_DEMAND_W = -1.0f;
-    const float SDM120M_MAXIMUM_TOTAL_SYSTEM_POWER_DEMAND_W = -1.0f;
-    const float SDM120M_IMPORT_SYSTEM_POWER_DEMAND_W = -1.0f;
-    const float SDM120M_MAXIMUM_IMPORT_SYSTEM_POWER_DEMAND_W = -1.0f;
-    const float SDM120M_EXPORT_SYSTEM_POWER_DEMAND_W = -1.0f;
-    const float SDM120M_MAXIMUM_EXPORT_SYSTEM_POWER_DEMAND = -1.0f;
-    const float SDM120M_CURRENT_DEMAND_A = -1.0f;
-    const float SDM120M_MAXIMUM_CURRENT_DEMAND_A = -1.0f;
+    const float SDM120M_IMPORT_ACTIVE_ENERGY_kWh= 0.0f;
+    const float SDM120M_EXPORT_ACTIVE_ENERGY_kWh= 0.0f;
+    const float SDM120M_IMPORT_REACTIVE_ENERGY_kVArh= 0.0f;
+    const float SDM120M_EXPORT_REACTIVE_ENERGY_kVArh= 0.0f;
+    const float SDM120M_TOTAL_SYSTEM_POWER_DEMAND_W= 0.0f;
+    const float SDM120M_MAXIMUM_TOTAL_SYSTEM_POWER_DEMAND_W= 0.0f;
+    const float SDM120M_IMPORT_SYSTEM_POWER_DEMAND_W= 0.0f;
+    const float SDM120M_MAXIMUM_IMPORT_SYSTEM_POWER_DEMAND_W= 0.0f;
+    const float SDM120M_EXPORT_SYSTEM_POWER_DEMAND_W= 0.0f;
+    const float SDM120M_MAXIMUM_EXPORT_SYSTEM_POWER_DEMAND= 0.0f;
+    const float SDM120M_CURRENT_DEMAND_A= 0.0f;
+    const float SDM120M_MAXIMUM_CURRENT_DEMAND_A= 0.0f;
     const float SDM120M_TOTAL_ACTIVE_ENERGY_kWh = 23.1;
-    const float SDM120M_TOTAL_REACTIVE_ENERGY_kVArh = -1.0f;
+    const float SDM120M_TOTAL_REACTIVE_ENERGY_kVArh= 0.0f;
 
-    uint32_t sendable = 0x00;
+    uint32_t sendable = 0x00000000;
     uint16_t address = (MasterMsg->mbData.dataNames.startAddressHi << 8) | (MasterMsg->mbData.dataNames.startAddressLo << 0);
+    float tempVar = 0.0f;
     switch(address)
     {
         case 0x0000:
         {
-            float tempVar = SDM120M_READ_VOLTAGE_V + 1.0f;
+            tempVar = SDM120M_READ_VOLTAGE_V;
             sendable = *((const uint32_t*) &tempVar);
             break;
         }
         case 0x0006:
         {
-            float tempVar = SDM120M_READ_CURRENT_A + 1.0f;
+            tempVar = SDM120M_READ_CURRENT_A;
             sendable = *((const uint32_t*) &tempVar);
             break;
         }
         case 0x000C:
         {
-            float tempVar = SDM120M_ACTIVE_POWER_W + 1.0f;
+            tempVar = SDM120M_ACTIVE_POWER_W;
             sendable = *((const uint32_t*) &tempVar);
             break;
         }
         case 0x0012:
         {
-            float tempVar = SDM120M_APPARENT_POWER_VA + 1.0f;
+            tempVar = SDM120M_APPARENT_POWER_VA;
             sendable = *((const uint32_t*) &tempVar);
             break;
         }
         case 0x0018:
         {
-            float tempVar = SDM120M_REACTIVE_POWER_VAr + 1.0f;
+            tempVar = SDM120M_REACTIVE_POWER_VAr;
             sendable = *((const uint32_t*) &tempVar);
             break;
         }
         case 0x001E:
         {
-            float tempVar = SDM120M_POWER_FACTOR + 1.0f;
+            tempVar = SDM120M_POWER_FACTOR;
             sendable = *((const uint32_t*) &tempVar);
             break;
         }
         case 0x0046:
         {
-            float tempVar = SDM120M_FREQUENCY + 1.0f;
+            tempVar = SDM120M_FREQUENCY;
             sendable = *((const uint32_t*) &tempVar);
             break;
         }
         case 0x0048:
         {
-            float tempVar = SDM120M_IMPORT_ACTIVE_ENERGY_kWh + 1.0f;
+            tempVar = SDM120M_IMPORT_ACTIVE_ENERGY_kWh;
             sendable = *((const uint32_t*) &tempVar);
             break;
         }
         case 0x004A:
         {
-            float tempVar = SDM120M_EXPORT_ACTIVE_ENERGY_kWh + 1.0f;
+            tempVar = SDM120M_EXPORT_ACTIVE_ENERGY_kWh;
             sendable = *((const uint32_t*) &tempVar);
             break;
         }
         case 0x004C:
         {
-            float tempVar = SDM120M_IMPORT_REACTIVE_ENERGY_kVArh + 1.0f;
+            tempVar = SDM120M_IMPORT_REACTIVE_ENERGY_kVArh;
             sendable = *((const uint32_t*) &tempVar);
             break;
         }
         case 0x004E:
         {
-            float tempVar = SDM120M_EXPORT_REACTIVE_ENERGY_kVArh + 1.0f;
+            tempVar = SDM120M_EXPORT_REACTIVE_ENERGY_kVArh;
             sendable = *((const uint32_t*) &tempVar);
             break;
         }
         case 0x0054:
         {
-            float tempVar = SDM120M_TOTAL_SYSTEM_POWER_DEMAND_W + 1.0f;
+            tempVar = SDM120M_TOTAL_SYSTEM_POWER_DEMAND_W;
             sendable = *((const uint32_t*) &tempVar);
             break;
         }
         case 0x0056:
         {
-            float tempVar = SDM120M_MAXIMUM_TOTAL_SYSTEM_POWER_DEMAND_W + 1.0f;
+            tempVar = SDM120M_MAXIMUM_TOTAL_SYSTEM_POWER_DEMAND_W;
             sendable = *((const uint32_t*) &tempVar);
             break;
         }
         case 0x0058:
         {
-            float tempVar = SDM120M_IMPORT_SYSTEM_POWER_DEMAND_W + 1.0f;
+            tempVar = SDM120M_IMPORT_SYSTEM_POWER_DEMAND_W;
             sendable = *((const uint32_t*) &tempVar);
             break;
         }
         case 0x005A:
         {
-            float tempVar = SDM120M_MAXIMUM_IMPORT_SYSTEM_POWER_DEMAND_W + 1.0f;
+            tempVar = SDM120M_MAXIMUM_IMPORT_SYSTEM_POWER_DEMAND_W;
             sendable = *((const uint32_t*) &tempVar);
             break;
         }
         case 0x005C:
         {
-            float tempVar = SDM120M_EXPORT_SYSTEM_POWER_DEMAND_W + 1.0f;
+            tempVar = SDM120M_EXPORT_SYSTEM_POWER_DEMAND_W;
             sendable = *((const uint32_t*) &tempVar);
             break;
         }
         case 0x005E:
         {
-            float tempVar = SDM120M_MAXIMUM_EXPORT_SYSTEM_POWER_DEMAND + 1.0f;
+            tempVar = SDM120M_MAXIMUM_EXPORT_SYSTEM_POWER_DEMAND;
             sendable = *((const uint32_t*) &tempVar);
             break;
         }
         case 0x0102:
         {
-            float tempVar = SDM120M_CURRENT_DEMAND_A + 1.0f;
+            tempVar = SDM120M_CURRENT_DEMAND_A;
             sendable = *((const uint32_t*) &tempVar);
             break;
         }
         case 0x0108:
         {
-            float tempVar = SDM120M_MAXIMUM_CURRENT_DEMAND_A + 1.0f;
+            tempVar = SDM120M_MAXIMUM_CURRENT_DEMAND_A;
             sendable = *((const uint32_t*) &tempVar);
             break;
         }
         case 0x0156:
         {
-            float tempVar = SDM120M_TOTAL_ACTIVE_ENERGY_kWh + 1.0f;
+            tempVar = SDM120M_TOTAL_ACTIVE_ENERGY_kWh;
             sendable = *((const uint32_t*) &tempVar);
             break;
         }
         case 0x0158:
         {
-            float tempVar = SDM120M_TOTAL_REACTIVE_ENERGY_kVArh + 1.0f;
-            sendable = *((const uint32_t*) &tempVar);
+            tempVar = SDM120M_TOTAL_REACTIVE_ENERGY_kVArh;
             break;
         }
         default:
@@ -245,6 +254,8 @@ static void modbus2ParserAndComposer(mb2MasterMsg* MasterMsg, mb2SlaveMsg* Slave
             break;
         }
     }
+    tempVar += MasterMsg->mbData.dataNames.slaveAddress;
+    sendable = *((const uint32_t*) &tempVar);
 
     // compose the message based on master message
     SlaveMsg->mbData.dataNames.slaveAddress = MasterMsg->mbData.dataNames.slaveAddress;
@@ -257,19 +268,61 @@ static void modbus2ParserAndComposer(mb2MasterMsg* MasterMsg, mb2SlaveMsg* Slave
     SlaveMsg->mbData.dataNames.CrcLo = 0;
     SlaveMsg->mbData.dataNames.CrcHi = 0;
 
-
+    // RAW DEBUG
     //uint8_t* ptr = &MasterMsg->mbData.dataArr[0];
     //char printable[100] = {0};
     //sprintf(printable, "Master: {%X, %X, %X, %X, %X, %X, %X, %X}\n", ptr[0], ptr[1], ptr[2], ptr[3], ptr[4], ptr[5], ptr[6], ptr[7]);
     //uartWrite(printable);
     //ptr = &SlaveMsg->mbData.dataArr[0];
-    //sprintf(printable, "Slave: {%X, %X, %X, %X, %X, %X, %X, %X, %X}\n\n", ptr[0], ptr[1], ptr[2], ptr[3], ptr[4], ptr[5], ptr[6], ptr[7], ptr[8]);
+    //sprintf(printable, "Slave: {%X, %X, %X, %X, %X, %X, %X, %X, %X}\n", ptr[0], ptr[1], ptr[2], ptr[3], ptr[4], ptr[5], ptr[6], ptr[7], ptr[8]);
     //uartWrite(printable);
+    //uint32_t temp = ((uint32_t)ptr[3] << 24) |
+    //                ((uint32_t)ptr[4] << 16) |
+    //                ((uint32_t)ptr[5] << 8)  |
+    //                ((uint32_t)ptr[6] << 0);
+    //    float result = *((float *)&temp); // convert uint32_t value to float variable
+    //sprintf(printable, "Value: %f\n\n", result);
+    //uartWrite(printable);
+
+    // UNIQUE MESSAGES ONLY
+    static mb2MasterMsg messagesArr[20*21] = {0};
+    static int lastMsgIndx = 0;
+    bool msgFound = false;
+    // go through all saved messages
+    for (int i = 0; i < lastMsgIndx; i++)
+    {
+        // if the message was already recorded
+        if (1 == compareMasterMsgs(MasterMsg, &messagesArr[i]))
+        {
+            msgFound = true;
+            break;  // break out
+        }
+    }
+    if (0 == msgFound)
+    {
+        // message wasn't found, store it and display it
+        messagesArr[lastMsgIndx++] = *MasterMsg;
+        uint8_t* ptr = &MasterMsg->mbData.dataArr[0];
+        char printable[100] = {0};
+        sprintf(printable, "Master: {%X, %X, %X, %X, %X, %X, %X, %X}\n", ptr[0], ptr[1], ptr[2], ptr[3], ptr[4], ptr[5], ptr[6], ptr[7]);
+        uartWrite(printable);
+        ptr = &SlaveMsg->mbData.dataArr[0];
+        sprintf(printable, "Slave: {%X, %X, %X, %X, %X, %X, %X, %X, %X}\n", ptr[0], ptr[1], ptr[2], ptr[3], ptr[4], ptr[5], ptr[6], ptr[7], ptr[8]);
+        uartWrite(printable);
+        uint32_t temp = ((uint32_t)ptr[3] << 24) |
+                        ((uint32_t)ptr[4] << 16) |
+                        ((uint32_t)ptr[5] << 8)  |
+                        ((uint32_t)ptr[6] << 0);
+            float result = *((float *)&temp); // convert uint32_t value to float variable
+        sprintf(printable, "Value: %f\n\n", result);
+        uartWrite(printable);
+    }
 }
 
 static void modbus2Responder(mb2SlaveMsg* SlaveMsg)
 {
     uart_write_bytes(UART_NUM_2, (const char*) SlaveMsg->mbData.dataArr, 9);
+    uart_flush(UART_NUM_2);
 }
 
 void modbus2_runnable(void)
